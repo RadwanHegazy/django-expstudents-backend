444f5f12a6566a6781b19bf6404418da724a7d10
RadwanHegazy <radwangaber22@gmail.com>
2024-06-11 15:06:59 +0300

    configure post app to system

--- backend/urls.py
+++ backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.apis.urls')),
+    path('post/', include('post.apis.urls')),
 ]
